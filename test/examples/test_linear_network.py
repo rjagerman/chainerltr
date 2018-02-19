e3e8f58fe1addb12bf4d07d0a23f3bafdea2b2cf
Rolf Jagerman <rjagerman@gmail.com>
2018-02-19 15:53:57 +0100

    Optimize hyperparameters for tests

--- test/examples/test_linear_network.py
+++ test/examples/test_linear_network.py
@@ -4,16 +4,16 @@ from test.examples.util import run_linear_network
 from chainer.testing import assert_allclose, condition
 
 
-def test_linear_listnet(alpha=0.3, batch_size=2):
+def test_linear_listnet(alpha=1.70, batch_size=4):
     final_loss = run_linear_network(listnet, alpha, batch_size)
     print(final_loss)
     assert_allclose(final_loss, 0.20512548089027405, atol=1e-2, rtol=1e-2)
 
 
-def test_linear_listmle(alpha=0.85, batch_size=1):
+def test_linear_listmle(alpha=0.90, batch_size=1):
     final_loss = run_linear_network(listmle, alpha, batch_size)
     print(final_loss)
-    assert_allclose(final_loss, 4.400527318318685, atol=1e-2, rtol=1e-2)
+    assert_allclose(final_loss, 4.307577768961589, atol=1e-2, rtol=1e-2)
 
 
 def test_linear_listpl(alpha=0.15, batch_size=1):
