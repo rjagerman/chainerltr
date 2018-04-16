5a8862e651eaa822378936b0811423eef64af8a5
Rolf Jagerman <rjagerman@gmail.com>
2018-04-16 17:49:26 +0200

    Improve phrasing in documentation of sample function

--- chainerltr/functions/sampling.py
+++ chainerltr/functions/sampling.py
@@ -25,10 +25,10 @@ def sample_without_replacement(p_log, seed=None):
     if seed is not None:
         rng = xp.random.RandomState(seed)
 
-    # We use reservoir sampling, which resorts to doing Uniform(0, 1) ^ (1 / p)
-    # and then sorting by the resulting values. The following implementation is
-    # a numerically stable variant that operates in log-space and uses
-    # GPU-accelerated operations.
+    # This uses reservoir sampling, which comes down to doing
+    # Uniform(0, 1) ^ (1 / p) and then sorting by the resulting values. The
+    # following implementation is a numerically stable variant that operates in
+    # log-space and uses GPU-accelerated operations.
     u = rng.uniform(0.0, 1.0, p_log.shape)
     r = xp.log(-xp.log(u)) - p_log.data
     s = xp.argsort(r, axis=1)
